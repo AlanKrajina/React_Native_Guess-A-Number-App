ae7976c8cf5e17f8858dc0e3c10d3eb5851513a8
Alan Krajina <alankrajina88@gmail.com>
2020-02-11 17:10:33 +0000

    added number confirmation

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -7,10 +7,35 @@ import Input from '../components/Input'
 const StartGameScreen = props => {
 
     const [enteredValue,setEnteredvalue] = useState('');
+    const [confirmed,setConfirmed] = useState(false);
+    const [selectedNumber,setSelectedNumber] = useState();
 
     const changeText = newNumb => {
         setEnteredvalue(newNumb.replace(/[^0-9]/g, '')) // non numberical number drop
     }
+
+    const resetValue = () => {
+        setEnteredvalue('');
+        setConfirmed(false)
+    }
+
+    const confirmValue = () => {
+        const chosenNumber = parseInt(enteredValue);
+        if (chosenNumber === NaN || chosenNumber <= 0 || chosenNumber > 99){
+            return; // if the value is invalid like here, return stops executing 3 functions bellow
+        }           // only numbers between 0-99, and no invalid numbers
+        setConfirmed(true);  // changes confirmed state
+        setSelectedNumber(parseInt(chosenNumber))
+        setEnteredvalue('');
+    }
+
+    let confirmedOutput;   // number for checking 
+
+    if (confirmed){   // if number OK via confirmValue(), confirmed === true
+    confirmedOutput= <Text>Chosen Number: {selectedNumber}</Text> // this will be outputed to screen
+    }
+
+
 
     return (
         <TouchableWithoutFeedback onPress={() => {
@@ -26,13 +51,14 @@ const StartGameScreen = props => {
                 />
                 <View style={styles.buttonContainer}>
                     <View style={styles.button} >
-                        <Button title='Confirm' onPress={()=> {}}/>
+                        <Button title='Confirm' onPress={confirmValue}/>
                     </View>
                     <View style={styles.button} >
-                        <Button title='Reset' onPress={()=> {}} color={Colors.primary}/>
+                        <Button title='Reset' onPress={resetValue} color={Colors.primary}/>
                     </View>
                 </View>
             </Card>
+            {confirmedOutput}
         </View>
         </TouchableWithoutFeedback>
     )
