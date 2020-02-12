9c8922edfab471068ec2759c65c1d4dbf8383579
Alan Krajina <alankrajina88@gmail.com>
2020-02-12 16:52:09 +0000

    added GameScreen logic to switch components on start

--- App.js
+++ App.js
@@ -1,14 +1,26 @@
-import React from 'react';
-import { StyleSheet, View , Text} from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, View } from 'react-native';
 import Header from './components/Header';
 import StartGameScreen from './screens/StartGameScreen';
+import GameScreen from './screens/GameScreen';
 
 
 export default function App() {
+
+  const [userNumber, setUserNumber] = useState();
+
+  const startGameHandler = (selectedNumber) => {
+    setUserNumber(selectedNumber)
+  }
+
+  let content = <StartGameScreen onStartGame={startGameHandler}/>;
+
+  userNumber ? content = <GameScreen userChoice={userNumber}/> : null
+
   return (
     <View style={styles.screen}>
       <Header title={'Guess A Number'}/>
-      <StartGameScreen />
+      {content}
     </View>
   );
 }
